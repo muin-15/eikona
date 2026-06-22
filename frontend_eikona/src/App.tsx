223b1f4108bcf2cec805edd2bed429414c48c765
muin-15 <muinmomin2@gmail.com>
2026-06-23 00:26:58 +0530

    degraded falsy changes from app.tsx

--- frontend_eikona/src/App.tsx
+++ frontend_eikona/src/App.tsx
@@ -4,17 +4,7 @@ function App() {
 
   return (
     <div>
-      const downloadImage = (base64Data: string, fileName: string) => {
-  // Create a temporary link element
-  const link = document.createElement("a");
-  link.href = base64Data; // This is your "data:image/jpeg;base64,..." string
-  link.download = fileName; // The name the file will have when saved
-  
-  // Append to body, click it, and remove it
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-};
+      
     </div>
   )
 }
