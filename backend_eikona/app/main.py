0881eae7009e7d48178cc7ca967a1b1d6d48484b
muin-15 <muinmomin2@gmail.com>
2026-06-23 01:46:20 +0530

    added gaussian image blurring in black and white,mean filtering in black and white

--- backend_eikona/app/main.py
+++ backend_eikona/app/main.py
@@ -1,8 +1,23 @@
 import cv2
 
-# Read image
-image = cv2.imread('lufy.png')
+
+image = cv2.imread('luffy.jpeg')
+
+
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imwrite('grayscale.jpg', gray)
-edges = cv2.Canny(gray, 100, 200)
+blurred_bw = cv2.GaussianBlur(gray, (5, 5), 0)
+gaussian_bw = cv2.GaussianBlur(gray, (5, 5), 0)
+mean_bw=cv2.boxFilter(gray, -1, (5, 5))
+blurred_color = cv2.GaussianBlur(image, (5, 5), 0)
+gaussian_color = cv2.GaussianBlur(image, (5, 5), 0)
+mean_color=cv2.boxFilter(image, -1, (5, 5))
+edges = cv2.Canny(blurred_bw, 100, 200)
+
 cv2.imwrite('edges.jpg', edges)
+cv2.imwrite('grayscale.jpg', gray)
+cv2.imwrite('blurred_bw.jpg', blurred_bw)
+cv2.imwrite('gaussian_bw.jpg', gaussian_bw)
+cv2.imwrite('mean_bw.jpg', mean_bw)
+cv2.imwrite('blurred_color.jpg', blurred_color)
+cv2.imwrite('gaussian_color.jpg', gaussian_color)
+cv2.imwrite('mean_color.jpg', mean_color)
