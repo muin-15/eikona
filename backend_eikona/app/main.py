bb2e5195e998ce07826fa173c2782f9f50588240
muin-15 <muinmomin2@gmail.com>
2026-06-23 01:11:36 +0530

    added grayscale feature

--- backend_eikona/app/main.py
+++ backend_eikona/app/main.py
@@ -3,5 +3,6 @@ import cv2
 # Read image
 image = cv2.imread('lufy.png')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imwrite('grayscale.jpg', gray)
 edges = cv2.Canny(gray, 100, 200)
 cv2.imwrite('edges.jpg', edges)
