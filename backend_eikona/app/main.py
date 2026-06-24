92113aea8c98d558292780daeb1a481697846caf
muin-15 <muinmomin2@gmail.com>
2026-06-24 20:11:33 +0530

    Replaced div with label enabling image input from the frontend

--- backend_eikona/app/main.py
+++ backend_eikona/app/main.py
@@ -1,4 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import cv2
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 image = cv2.imread('luffy.jpeg')
 
